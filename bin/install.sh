ca3acfa7ed9d45baf37e44a984a7b2a206e2b9ff
usajusaj <usajusaj@usajusaj.org>
2020-12-01 23:38:23 -0500

    Handle Python 2.6 in install script

--- bin/install.sh
+++ bin/install.sh
@@ -1,15 +1,28 @@
 #!/bin/bash
 
+PIP_26="https://bootstrap.pypa.io/2.6/get-pip.py"
+PIP="https://bootstrap.pypa.io/get-pip.py"
+
 # Check if pip exists on this server
 if ! command -v pip &>/dev/null; then
   # Bootstrap pip it if not present
-  echo "Pip not found, installing from https://bootstrap.pypa.io/get-pip.py"
-  curl -O https://bootstrap.pypa.io/get-pip.py
+  PY_VER=$(python -c "import sys; print('2.6' if sys.version_info[0] == 2 and sys.version_info[1] < 7 else '2.7')")
+  PIP_URL="$PIP"
+
+  if test PY_VER = "2.6"; then
+    PIP_URL="$PIP_26"
+  fi
+
+  echo "Pip not found, installing from $PIP_URL"
+  curl -O "$PIP_URL"
 
   if ! python get-pip.py &>/dev/null; then
     echo "Error installing pip"
+    rm get-pip.py
     exit 1
   fi
+
+  rm get-pip.py
 fi
 
 # Install via zip, git package not required by doing this
@@ -21,8 +34,7 @@ fi
 INSTALL_DIR=$(python -c "import ccbr_server, os; print(os.path.dirname(ccbr_server.__file__))")
 DEFAULT_CONF="$INSTALL_DIR/etc/ccbr_scripts.ini"
 
-if test -f /etc/ccbr_scripts.ini
-then
+if test -f /etc/ccbr_scripts.ini; then
   echo "Config ini exists: /etc/ccbr_scripts.ini"
   echo "Check for new config vars in $DEFAULT_CONF"
 else
